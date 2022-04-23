5a33bff4419234778b9cc781c4a27e6a8123bd6a
Zied Oussaifi <ousaifizied@hotmail.fr>
2022-04-23 19:37:32 +0200

    removing doublons

--- app/models/list 2.rb
+++
@@ -1,8 +0,0 @@
-class List < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
-  has_many :movies, through: :bookmarks
-  has_many :reviews, dependent: :destroy
-  has_one_attached :photo
-
-  validates :name, uniqueness: true, presence: true
-end
--- app/models/movie 2.rb
+++
@@ -1,6 +0,0 @@
-class Movie < ApplicationRecord
-  has_many :bookmarks
-
-  validates :title, presence: true, uniqueness: true
-  validates :overview, presence: true
-end
